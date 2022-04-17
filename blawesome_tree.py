faa85f30f47c8d9c47fd03875cafea3341e4d23f
AJS0191 <ashtonjamess@live.com>
2022-04-17 10:10:13 -0500

    ADD:Blossom class UPDATE:fruit falling from fixed

--- blawesome_tree.py
+++ blawesome_tree.py
@@ -9,6 +9,10 @@ pygame.init()
 
 # displaying a window of height
 display_surface = pygame.display.set_mode((850, 600))
+
+# set the clock
+clock = pygame.time.Clock()
+currentTime = 0
 # screen = pygame.display.set_mode((850, 600))
 # Making background image
 # image = pygame.image.load('tree.png')
@@ -28,32 +32,57 @@ class Fruit(pygame.sprite.Sprite):
         # create the sprite and fill with color (update with images)
         self.image = pygame.Surface([width, height])
         self.image.fill(color)
+        self.color = color
+        self.width = width
+        self.height = height
         # set start position of fruit with random
         self.rect = self.image.get_rect()
         self.rect.x = random.randrange(30, 820)
         self.rect.y = random.randrange(30, 300)
         self.speed = speed
 
+    # def update(self):
+        # self.rect.y = self.rect.y + self.speed
+
+
+# adding the blossom class for falling objects from fruit ((i think i named this backwards))
+class Blossom(pygame.sprite.Sprite):
+    def __init__(self, fruit):
+        super().__init__()
+        self.color = WHITE  # change to fruit.color
+        self.width = fruit.width
+        self.height = fruit.height
+        self.image = pygame.Surface([self.width, self.height])
+        self.image.fill(self.color)
+        self.speed = random.randrange(1, 4)
+        self.rect = self.image.get_rect()
+        self.rect.x = fruit.rect.x
+        self.rect.y = fruit.rect.y
+
     def update(self):
         self.rect.y = self.rect.y + self.speed
 
-
 # create a list to hold the fruit sprites
 fruitList = pygame.sprite.Group()
+# create a list to hold the blossom sprites
+blossomList = pygame.sprite.Group()
 # create a list that holds all sprites
 spriteList = pygame.sprite.Group()
 
-# create the fruits
+# create the fruits and the blossoms
 numberFruits = 10
 for i in range(0, numberFruits):
     blawesomeFruit = Fruit(10, 10, GREEN, 2)
+    blawesomeBlossom = Blossom(blawesomeFruit)
     fruitList.add(blawesomeFruit)
+    blossomList.add(blawesomeBlossom)
     spriteList.add(blawesomeFruit)
-fruit_y = 10
-fruit_x = 10
+    spriteList.add(blawesomeBlossom)
+# fruit_y = 10
+# fruit_x = 10
 #displaying tree to background
 #screen.fill(color)
-clock = pygame.time.Clock()
+
 
 # creating a bool value which checks
 # if game is running
@@ -70,6 +99,8 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
+    # update the current time
+    currentTime = pygame.time.get_ticks()
     # game logic can go here
     spriteList.update()
     # fill the background of the game
@@ -78,7 +109,7 @@ while running:
     # draw to the screen
 
     spriteList.draw(display_surface)
-    # pygame.draw.rect(display_surface, color, (fruit_x, fruit_y, 20, 20))
+    # 
     # pygame.draw.rect(display_surface, color, (fruit_x + 70, fruit_y + 10, 20, 20))
     # pygame.draw.rect(display_surface, color, (fruit_x + 180, fruit_y + 18, 20, 20))
     # display_surface.blit(image,(0,0))
